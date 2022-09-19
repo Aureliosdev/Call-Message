235dc438f035d7777394bad277202c2dcfc138c7
Abylay <aurelioleclarke@gmail.com>
2022-09-19 14:02:46 +0600

    Twitter 1.0
    Created

---
+++ Twitter UIKit/Twitter UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Twitter UIKit
+//
+//  Created by Aurelio Le Clarke on 19.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
